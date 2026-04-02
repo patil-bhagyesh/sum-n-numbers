649eff4c3d069102968f81e461c3e1ed6fa316a8
patil-bhagyesh <bhagyesh.patil@gmail.com>
2026-04-02 17:53:09 +0530

    Add sum_numbers.py file with function to sum numbers

---
+++ sum_numbers.py
@@ -0,0 +1,11 @@
+def sum_n_numbers(*args):
+    """Sum n numbers passed as arguments."""
+    if not args:
+        return 0
+    return sum(args)
+
+def sum_list(numbers):
+    """Sum a list of numbers."""
+    if not numbers:
+        return 0
+    return sum(numbers)
